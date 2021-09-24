2395b7c025de03a79163e1529b490a3df4c10123
yinyacelebi <85402436+yinyacelebi@users.noreply.github.com>
2021-09-24 21:23:38 +0800

    ?

--- src/main/java/com/elec/controller/DemoController.java
+++ src/main/java/com/elec/controller/DemoController.java
@@ -1,12 +1,13 @@
 package com.elec.controller;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("elec/outTake")
 public class DemoController {
-
+    @CrossOrigin
     @RequestMapping("/test")
     public String test(){
         return "启动成功";

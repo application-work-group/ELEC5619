9d5fa38fbf9bc2b6340c36eb52901206404bc8fa
WangJinglin <714567706@qq.com>
2021-10-08 20:41:01 +0800

    add: program

--- src/main/java/com/elec/dal/pojo/GameSession.java
+++ src/main/java/com/elec/dal/pojo/GameSession.java
@@ -4,7 +4,6 @@ import com.baomidou.mybatisplus.annotation.IdType;
 import com.baomidou.mybatisplus.annotation.TableId;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
-import lombok.experimental.Accessors;
 
 import java.io.Serializable;
 import java.util.Date;
@@ -15,14 +14,14 @@ import java.util.Date;
  * </p>
  *
  * @author Jinglin
- * @since 2021-09-08
+ * @since 2021-10-08
  */
 @Data
-@EqualsAndHashCode()
-@Accessors(chain = true)
+@EqualsAndHashCode(callSuper = false)
 public class GameSession implements Serializable {
 
     private static final long serialVersionUID = 1L;
+
     @TableId(value = "id", type = IdType.AUTO)
     private Integer id;
 

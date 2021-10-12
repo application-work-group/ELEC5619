bff427c24ce8e3232b7b0046253ec81ef08f360f
Shuyuan Liu <417059094@qq.com>
2021-10-12 23:35:21 +0800

    1
    1

--- src/main/java/com/elec/controller/GameSessionController.java
+++ src/main/java/com/elec/controller/GameSessionController.java
@@ -18,9 +18,13 @@ import org.springframework.web.bind.annotation.RestController;
 @Controller
 @RequestMapping("/game-session")
 public class GameSessionController {
+
     @RequestMapping("/test0shuyuan")
     public String test0shuyuan(Model model){
+
         model.addAttribute("name","zhangsan");
+
+
         return "test";
     }
     @RequestMapping("/test1shuyuan")

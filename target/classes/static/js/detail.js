0caf65ade36d34412d503916f98cfbca439b509e
Shuyuan Liu <417059094@qq.com>
2021-10-17 15:48:03 +0800

    1
    1

---
+++ target/classes/static/js/detail.js
@@ -0,0 +1,21 @@
+$(document).ready(function(){
+    $(".team_a_button").click(function(){
+        $(".item1").text($(this).find("i").text());
+        $(".item2").text($(this).find("b").text());
+        $(".window_head span").text($(this).parent().prev().find(".title_name").text());
+        $(".predict_window").show();
+    });
+    $(".team_b_button").click(function(){
+        $(".item1").text($(this).find("i").text());
+        $(".item2").text($(this).find("b").text());
+        $(".window_head span").text($(this).parent().prev().find(".title_name").text());
+        $(".predict_window").show();
+    });
+    $(".window_button_1").click(function(){
+        $(".predict_window").hide();
+    });
+    $("#predict_number").bind('input propertychange',function(){
+        var prize = $(this).val()*$(".item2").text();
+        $(".item3_win").text(prize);
+    });
+});
